1cebe5e439c02b26732159b3fb183d420cc161b4
Mateusz <nowe7246@gmail.com>
2022-11-12 11:59:08 +0100

    Added model of restaurant schema.

---
+++ Modals/Restaurant.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const restaurantSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            trim: true,
+            required: [true, 'name is required'],
+        },
+        address: {
+            street: {type: String, required: true},
+            building_number: {type: String, required: true},
+            zip_code: {type: String, required: true},
+            city: {type: String, required: true},
+        },
+        available_seats: {
+            type: Number,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+    },
+    {timestamps: true}
+)
+
+export const RestaurantSchema = mongoose.model('Seat', restaurantSchema)
