1923e92628065a2403af1040a40863330ee52344
Mateusz <nowe7246@gmail.com>
2022-11-13 12:07:43 +0100

    Created user types.

---
+++ types/user/user.types.ts
@@ -0,0 +1,9 @@
+export type UserTypes = {
+    username: string;
+    first_name: string,
+    last_name: string,
+    email: string,
+    password: string,
+    isAdmin: boolean,
+    favorite_places: string[],
+}
