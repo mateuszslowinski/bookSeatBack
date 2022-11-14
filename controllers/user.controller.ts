c4872ccb39974a621f0e1a5e1a8ccc32290477cd
Mateusz <nowe7246@gmail.com>
2022-11-14 17:46:24 +0100

    Added generate token function during register.

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -3,41 +3,54 @@ import {ValidationError} from "../utils/error";
 import bcrypt from 'bcrypt';
 import {User} from "../models/User";
 import {validatePassword} from "../utils/validation";
+import {generateToken} from "../utils/generateToken";
 
 export const userRegister = async (req: Request, res: Response) => {
     const {username, email, password, first_name, last_name, favorite_places} = req.body;
 
     const checkEmail = await User.findOne({email});
     if (checkEmail) {
-         throw new ValidationError('Email is already taken');
+        throw new ValidationError('Email is already taken');
     }
     const isCorrectPassword = validatePassword(password)
-    if(!isCorrectPassword){
+    if (!isCorrectPassword) {
         throw new ValidationError('password contains at least eight characters, including at least one number' +
             ' and includes both lower and uppercase letters and special characters')
     }
 
     try {
-    const salt = await bcrypt.genSalt(10);
-    const encryptedPassword = await bcrypt.hash(password, salt)
+        const salt = await bcrypt.genSalt(10);
+        const encryptedPassword = await bcrypt.hash(password, salt)
 
         const user = await new User({
             username,
             email,
             password: encryptedPassword,
-            isAdmin: false,
+            isAdmin: email === process.env.ADMIN_EMAIL,
             favorite_places,
             first_name,
             last_name,
         }).save();
-        res.status(201).json(user)
+
+        const token = generateToken({id: String(user._id)}, '14d')
+        res.status(201).json({
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            favorite_places: user.favorite_places,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            token: token,
+
+        })
     } catch (e) {
         throw new ValidationError(e.message);
     }
 }
 
 export const userLogin = async (req: Request, res: Response) => {
-    const { email, password } = req.body
+    const {email, password} = req.body
     const user = await User.findOne({email})
 
     if (!user) {
