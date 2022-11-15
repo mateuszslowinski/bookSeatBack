d5b0b9166bfaaa68a1e4db057d1e97d09a50d4aa
Mateusz <nowe7246@gmail.com>
2022-11-15 16:11:31 +0100

    Added authorization to getting a user profile.

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import {getUserProfile, removeProfile, updateDetails, userLogin, userRegister} from "../controllers/user.controller";
+import {protect} from "../middleware/auth.middleware";
 
 export const userRoute = express.Router()
 
 userRoute
     .post('/register', userRegister)
     .post('/login', userLogin)
-    .get('/profile', getUserProfile)
+    .get('/profile',protect, getUserProfile)
     .patch('/profile', updateDetails)
     .delete('/profile', removeProfile)
