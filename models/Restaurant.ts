b0dce4734643cbac9640eebc1ed1bfd385029572
Mateusz <nowe7246@gmail.com>
2022-11-15 19:30:35 +0100

    Fixed enum type of restaurant property.

--- models/Restaurant.ts
+++ models/Restaurant.ts
@@ -21,7 +21,8 @@ const restaurantSchema = new mongoose.Schema({
             required: true,
         },
         typeOfRestaurant: {
-            type: TypeOfRestaurant,
+            type:String,
+            enum: TypeOfRestaurant,
             required: true,
         },
         description: {
